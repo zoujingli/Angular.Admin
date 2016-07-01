beb7b2f1e81c821d6f0820e58cd7417d0f8b1d00
Anyon <zoujingli@qq.com>
2016-07-01 13:35:57 +0800

    实现自动加载机制

--- pages/welcome/hello.js
+++ pages/welcome/hello.js
@@ -1,8 +1,5 @@
 define(['angular'], function (angular) {
     return angular.module('app.welcome', []).run(function ($rootScope) {
-        $rootScope.message = '3423432';
+        $rootScope.message = '别着急嘛 ... ^_^ ... Angluar.Admin 正在工厂加班加点进行拼装...';
     });
-//     aa.run(['$rootScope', function ($rootScope) {
-//            $rootScope.message = '222';
-//        }]);
 });
