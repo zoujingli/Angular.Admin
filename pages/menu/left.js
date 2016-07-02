7166817a3928dc005f5e22c2a32eee6b96765378
Anyon <zoujingli@qq.com>
2016-07-02 04:56:49 -0400

    应用Cookies记录菜单状态

--- pages/menu/left.js
+++ pages/menu/left.js
@@ -1,5 +1,9 @@
-define(['app'], function (app) {
-    app.controller('app.menu', function ($rootScope) {
+define(['app', 'angular-cookies'], function (app) {
+
+    app.useModule('ngCookies');
+
+    app.controller('app.menu', function ($rootScope, $scope, $cookies) {
+
         $rootScope.toggleLeftNav = function () {
             var menuClass = $rootScope.app.layout.class.main;
             if (menuClass.indexOf(' framework-sidebar-full') !== -1) {
@@ -7,6 +11,9 @@ define(['app'], function (app) {
             } else {
                 $rootScope.app.layout.class.main = menuClass.replace(' framework-sidebar-mini', ' framework-sidebar-full');
             }
+        };
+        $scope.toggleLeftItemNav = function (menu) {
+            $cookies.put('menu-open-' + menu.node, menu.open = !menu.open);
         };
     });
 
--- pages/menu/top.js
+++ pages/menu/top.js
@@ -1,6 +1,8 @@
-define(['app'], function (app) {
+define(['angular', 'app', 'angular-cookies'], function (angular, app) {
 
-    app.controller('app.menu.top', function ($rootScope, $http) {
+    app.useModule('ngCookies');
+
+    app.controller('app.menu.top', function ($rootScope, $scope, $http, $cookies) {
         // 加载应用数据
         $http.get('server/app.json').success(function (ret) {
             $rootScope.appInfo = ret;
@@ -10,11 +12,51 @@ define(['app'], function (app) {
             $rootScope.userInfo = ret;
         });
         // 加载菜单信息
+        $rootScope.app.leftmenudata = false;
+
         $http.get('server/menu.json').success(function (ret) {
             $rootScope.app.layout.loaded = true;
-            $rootScope.menuTopInfo = ret;
-            console.log($rootScope.app);
+            setMenuStat($rootScope.app.menudata = ret);
         });
+
+        $scope.setLeftMenu = function (menu) {
+            angular.forEach($rootScope.app.menudata, function (menu) {
+                menu.active = false;
+                $cookies.put('menu-active-' + menu.node, false);
+            });
+            menu.active = true;
+            $cookies.put('menu-active-' + menu.node, true);
+            if (menu.sub) {
+                setMenuStat($rootScope.app.leftmenudata = menu.sub);
+            } else {
+                $rootScope.app.leftmenudata = false;
+            }
+        };
+
+
+
+        /**
+         * 处理菜单状态
+         * @param {type} menus
+         * @returns {undefined}
+         */
+        function setMenuStat(menus) {
+            menus && angular.forEach(menus, function (menu) {
+                var statType = ['active', 'open'];
+                for (var i in statType) {
+                    var type = statType[i];
+                    switch ($cookies.get('menu-' + type + '-' + menu.node)) {
+                        case 'true':
+                            menu[type] = true;
+                            break;
+                        case 'false':
+                            menu[type] = false;
+                            break;
+                    }
+                }
+                menu.sub && setMenuStat(menu.sub);
+            });
+        }
     });
 
 });
