0098bb151d2398603389c8ec92aa195f840d8440
Anyon <zoujingli@qq.com>
2016-07-05 00:21:15 +0800

    no commit message

--- pages/login/in.js
+++ pages/login/in.js
@@ -1,5 +1,9 @@
 define(['app'], function (app) {
-    app.controller('app.login', function ($rootScope) {
+    app.controller('app.login', function ($rootScope, $http) {
         $rootScope.app.layout.class.body = 'gray-bg full-height full-width login-container';
+        // 加载应用数据
+        $rootScope.appInfo || $http.get('server/app.json').success(function (ret) {
+            $rootScope.appInfo = ret;
+        });
     });
 });
