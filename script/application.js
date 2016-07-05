dede5590d2731a6d56fbcc06da9180fc1a5b76a5
Anyon <zoujingli@qq.com>
2016-07-05 15:19:07 +0800

    loading提前加载

--- script/application.js
+++ script/application.js
@@ -1,27 +1,6 @@
-define(['require', 'angular', 'angular-ui-router', 'angular-ui-bootstrap', 'angular-cookies', 'spin'], function (require, angular) {
+define(['require', 'angular', 'angular-ui-router', 'angular-ui-bootstrap', 'angular-cookies'], function (require, angular) {
 
-    new Spinner({
-        lines: 17 // The number of lines to draw
-        , length: 0 // The length of each line
-        , width: 11 // The line thickness
-        , radius: 41 // The radius of the inner circle
-        , scale: 1.75 // Scales overall size of the spinner
-        , corners: 1 // Corner roundness (0..1)
-        , color: '#000' // #rgb or #rrggbb or array of colors
-        , opacity: 0.05 // Opacity of the lines
-        , rotate: 0 // The rotation offset
-        , direction: 1 // 1: clockwise, -1: counterclockwise
-        , speed: 1 // Rounds per second
-        , trail: 60 // Afterglow percentage
-        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-        , zIndex: 2e9 // The z-index (defaults to 2000000000)
-        , className: 'display-inline-block' // The CSS class to assign to the spinner
-        , top: '50%' // Top position relative to parent
-        , left: '50%' // Left position relative to parent
-        , shadow: true // Whether to render a shadow
-        , hwaccel: true // Whether to use hardware acceleration
-        , position: 'fixed' // Element positioning
-    }).spin(document.getElementById('preloader'));
+   
 
     /**
      * 定义模块
