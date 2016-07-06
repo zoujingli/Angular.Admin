69e5e822b26a1acb5e07c69c6efa82a29ddc959b
Anyon <zoujingli@qq.com>
2016-07-06 23:27:22 +0800

    no commit message

--- pages/table/default.js
+++ pages/table/default.js
@@ -1,6 +1,10 @@
 define(['app.admin'], function (app) {
-    app.controller('app.table', function ($scope) {
+    app.controller('app.table', function ($scope, $http) {
         $scope.message = '表格';
+        $http.get('server/table-data.json').success(function (ret) {
+            console.log(ret);
+            $scope.data = ret;
+        })
     });
 
 });
