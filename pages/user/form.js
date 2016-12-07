53d205ebd34e63b8d42bb5c057e65a0639830048
Anyon <zoujingli@qq.com>
2016-12-07 05:59:17 -0500

    修改表单自动处理

--- pages/user/form.js
+++ pages/user/form.js
@@ -10,6 +10,10 @@ define(['require', 'angular', 'jquery', 'myView', 'myForm'], function (require, 
         /*! 创建默认模块及默认控制器 */
         angular.module(module, ['myView', 'myForm']).controller(controller, ['$scope', '$location', '$view', function ($scope, $location, $view) {
                 $scope.app.layout.class.body = 'body';
+                $scope.form = {};
+                $scope.form.username = '';
+                $scope.form.title = '默认值-只能在 controller 中定义哦！';
+                $scope.form.username = '';
             }
         ]);
     };
