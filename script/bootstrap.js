86c30eaaf4fbebdda972abd3186c35fe1bd5b6af
Anyon <zoujingli@qq.com>
2016-12-09 03:46:13 -0500

    修改备注

--- script/bootstrap.js
+++ script/bootstrap.js
@@ -1,4 +1,8 @@
 /* global require, layui */
+
+const API_URL = 'http://localhost/service/public/index.php/';
+const API_AUTH = 'user/api/token.html';
+
 var script = document.scripts[document.scripts.length - 1].src, baseUrl = script.substring(0, script.lastIndexOf("/"));
 
 /**
@@ -38,13 +42,13 @@ require.config({
         'debug': {deps: ['jquery']},
         'myView': {deps: ['angular']},
         'myForm': {deps: ['angular', 'myDialog', 'jquery']},
-        'myDialog': {deps: ['angular', 'sweetalert']},
+        'myDialog': {deps: ['angular', 'layui']},
         'ngRoute': {deps: ['angular']},
         'ngCookies': {deps: ['angular']},
         'ngSanitize': {deps: ['angular']},
     },
     deps: ['angular', 'css!script/plugs/layui/css/layui.css', 'css!theme/css/animate.css', 'css!theme/css/common.css'],
-//    urlArgs: "v=" + (new Date()).getTime()
+    urlArgs: "v=" + (new Date()).getTime()
 });
 
 /**
@@ -60,21 +64,35 @@ require(['pace'], function (pace) {
  * @param {type} angular
  * @returns {undefined}
  */
-require(['angular', 'ngRoute', 'myView', 'layui'], function (angular) {
+require(['angular', 'ngRoute', 'ngCookies', 'myView', 'myDialog', 'layui'], function (angular) {
+
+    // Layui 初始化
+    layui.config({dir: baseUrl + '/plugs/layui/'});
+    layui.use(['layer', 'laydate']);
 
     // 创建APP应用
-    var app = angular.module('app', ['ngRoute', 'myView']);
+    var app = angular.module('app', ['ngRoute', 'myView', 'myDialog', 'ngCookies']);
 
     // 应用启动配置
-    app.config(['$routeProvider', '$viewProvider', function ($routeProvider, $viewProvider) {
+    app.config(['$routeProvider', '$viewProvider', '$locationProvider', function ($routeProvider, $viewProvider, $locationProvider) {
+            $locationProvider.html5Mode(false);
             $viewProvider.registerView('login.html');
             $routeProvider.otherwise('login.html');
         }]);
 
     // 应用初始化动作
-    app.run(['$location', '$view', '$rootScope', '$templateCache', function ($location, $view, $rootScope, $templateCache) {
+    app.run(['$location', '$view', '$rootScope', '$templateCache', '$cookies', '$http', '$httpParamSerializerJQLike', '$dialog',
+        function ($location, $view, $rootScope, $templateCache, $cookies, $http, $httpParamSerializerJQLike, $dialog) {
 
+            // 模块初始化赋值
+            angular.$view = $view;
+            angular.$http = $http;
+            angular.$dialog = $dialog;
+            angular.$location = $location;
+            angular.$cookies = $cookies;
+            angular.$rootScope = $rootScope;
             $rootScope.$location = $location;
+            angular.$httpParamSerializerJQLike = $httpParamSerializerJQLike;
 
             // 页面全局属性定义
             $rootScope.app = {
@@ -89,12 +107,29 @@ require(['angular', 'ngRoute', 'myView', 'layui'], function (angular) {
                 }
             };
 
+            // 全局退出登录
+            $rootScope.logout = function () {
+                $dialog.confirm('确定要退出登录吗？', function () {
+                    $cookies.remove('token');
+                    $view.goto('login.html');
+                });
+            };
+
             // 页面跳转前的处理
             $rootScope.$on("$locationChangeStart", function () {
+                console.log('Start : ' + $location.$$path);
                 if ($location.$$path.length > 0) {
+                    // 动态注册路由
                     $view.registerView($location.$$path);
+                    // 需要登录的场景
+                    if ($location.$$path !== '/login.html' && !angular.$cookies.get('token')) {
+                        $dialog.tips('抱歉，需要登录后才能进入！');
+                        $view.goto('login.html');
+                    }
                 }
             });
+
+            // 页面跳转成功后清除缓存
             $rootScope.$on('$locationChangeSuccess', function () {
                 $templateCache.removeAll();
             });
@@ -113,9 +148,8 @@ require(['angular', 'ngRoute', 'myView', 'layui'], function (angular) {
                 document.body.appendChild(iframe);
             });
 
-        }]);
+        }
+    ]);
     // 启动应用
-    layui.config({dir: baseUrl + '/plugs/layui/'});
     angular.bootstrap(document, [app.name]);
-
 });
